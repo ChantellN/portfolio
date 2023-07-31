c9067bcc9852e8f1c5c6d90a44bc3a9e95647c58
Chantell Nichols <nicholsc1009@gmail.com>
2023-07-31 15:19:43 -0400

    floating nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,69 @@
-import React from "react";
 import Navbar from "./sections/navbar/Navbar";
 import Header from "./sections/header/Header";
 import Services from "./sections/services/Services";
 import About from "./sections/about/About";
 import Portfolio from "./sections/portfolio/Portfolio";
-import Testimonials from "./sections/testimonials/Testimonials";
-import FAQS from "./sections/faqs/FAQS";
 import Contact from "./sections/contact/Contact";
 import Footer from "./sections/footer/Footer";
 import FloatingNav from "./sections/floating-nav/FloatingNav";
+import Theme from "./theme/Theme";
+import { useThemeContext } from "./context/theme-context";
+import { useRef, useState, useEffect } from "react";
 
 const App = () => {
+  const { themeState } = useThemeContext();
+
+  const mainRef = useRef();
+  const [showFloatingNav, setShowFloatingNav] = useState(true);
+  const [siteYPosition, setSiteYPosition] = useState(0);
+
+  const showFloatingNavHandler = () => {
+    setShowFloatingNav(true);
+  };
+
+  const hideFloatingNavHandler = () => {
+    setShowFloatingNav(false);
+  };
+
+  // check if floating nav should be shown or hidden
+  // eslint-disable-next-line
+  const floatingNavToggleHandler = () => {
+    // check if we scrolled up or down at least 20px
+    if (
+      siteYPosition < mainRef?.current?.getBoundingClientRect().y - 20 ||
+      siteYPosition > mainRef?.current?.getBoundingClientRect().y + 20
+    ) {
+      showFloatingNavHandler();
+    } else {
+      hideFloatingNavHandler();
+    }
+
+    setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
+  };
+
+  useEffect(
+    (floatingNavToggleHandler) => {
+      const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+      // cleanup function
+      return () => clearInterval(checkYPosition);
+    },
+    [siteYPosition]
+  );
+
   return (
-    <main className={`${themeState.primary} ${themeState.background}`}>
+    <main
+      className={`${themeState.primary} ${themeState.background}`}
+      ref={mainRef}
+    >
       <Navbar />
       <Header />
       <About />
       <Services />
       <Portfolio />
-      <Testimonials />
-      <FAQS />
       <Contact />
       <Footer />
-      <FloatingNav />
+      <Theme />
+      {showFloatingNav && <FloatingNav />}
     </main>
   );
 };
